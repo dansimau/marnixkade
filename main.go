6a4fc8e40d7ceb63fa088762ddd4f4e3ec363ef2
Daniel Simmons <dan@dans.im>
2024-12-11 12:45:35 +0100

    Add automation for study lights

--- main.go
+++ main.go
@@ -13,11 +13,22 @@ type Marnixkade struct {
 	*hal.Connection
 
 	Hallway Hallway
+	Study   Study
+}
+
+func (m *Marnixkade) TestFunc() {
+	slog.Info("TestFunc")
 }
 
 type Hallway struct {
 	Lights       *hal.Light
 	MotionSensor *hal.Entity
+}
+
+type Study struct {
+	Lights         *hal.Light  // Main lights
+	LuxSensor      *hal.Entity // Aqara FP2
+	PresenceSensor *hal.Entity // Aqara FP2
 }
 
 func NewMarnixkade() *Marnixkade {
@@ -33,6 +44,11 @@ func NewMarnixkade() *Marnixkade {
 			Lights:       hal.NewLight("light.front_hallway"),
 			MotionSensor: hal.NewEntity("binary_sensor.hallway_motion"),
 		},
+		Study: Study{
+			Lights:         hal.NewLight("light.study_lights"),
+			PresenceSensor: hal.NewEntity("binary_sensor.presence_sensor_fp2_11ad_presence_sensor_1"),
+			LuxSensor:      hal.NewEntity("sensor.presence_sensor_fp2_11ad_light_sensor_light_level"),
+		},
 	}
 
 	// Walk the struct and find/register all entities
@@ -44,9 +60,19 @@ func NewMarnixkade() *Marnixkade {
 			WithName("Hallway lights").
 			WithSensors(home.Hallway.MotionSensor).
 			WithLights(home.Hallway.Lights).
-			TurnsOffAfter(5 * time.Minute),
-	)
+			TurnsOffAfter(5*time.Minute),
 
+		halautomations.NewSensorsTriggersLights().
+			WithName("Study lights").
+			WithSensors(home.Study.PresenceSensor).
+			WithLights(home.Study.Lights).
+			TurnsOffAfter(5*time.Minute),
+
+		// hal.NewAutomation().
+		// 	WithName("Hallway lights").
+		// 	WithEntities(home.Hallway.MotionSensor).
+		// 	WithAction(home.TestFunc),
+	)
 	return home
 }
 
